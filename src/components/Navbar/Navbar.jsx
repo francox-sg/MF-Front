b8dfb182f509163ce44ac9d07a9ee64c49c5c063
Franco Galeano <francoxsg@gmail.com>
2025-01-03 21:21:03 -0300

    Barra de tareas finalizada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import classes from './Navbar.module.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () =>{
+const [showNavbar, setShowNabvar] = useState(false)
+
+
 
     return(
-        <div className={classes.navbar}>
-            <div className={classes.logo}> 
+        <div className={classes.navbar} style={showNavbar?{width:150}:{width:40}} onMouseMove={()=>{setShowNabvar(true)}} onMouseLeave={()=>{setShowNabvar(false)}}>
+            <div className={classes.logo} style={showNavbar?{right:-60}:{right:-90}}> 
                 <img src={logo} alt="" />
             </div>
-            <ul className={classes.navItems}>
-                <Link to="/" className={classes.navLink}>Pacientes</Link>
+            <ul className={classes.navItems} style={showNavbar?{paddingRight:0}:{paddingRight:100}}>
+                <Link to="/" className={classes.navLink} style={showNavbar?{}:{color:"rgba(43, 38, 38, 0)"}} >Pacientes</Link>
             </ul>
         </div>
     )
